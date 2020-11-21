b4acd650df32fc044648e0cf55d5ced848c907b2
Agnaldo Burgo Junior <agnaldoburgojr@gmail.com>
2020-11-21 09:03:25 -0300

    feat: render kinds in contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,7 +5,14 @@ class Contact < ApplicationRecord
     "Agnaldo Burgo Junior"
   end
 
+  def kind_description
+    self.kind.description
+  end
+
   def as_json(options={})
-    super(methods: :author)
+    super(
+      methods: [:author, :kind_description],
+      include: { kind: { only: :description} }
+    )
   end
 end

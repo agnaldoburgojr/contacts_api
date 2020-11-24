930131d0107298076dbd5363198f1d980b35c8b4
Agnaldo Burgo Junior <agnaldoburgojr@gmail.com>
2020-11-24 06:57:12 -0300

    feat: add kinds in contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,18 +1,18 @@
 class Contact < ApplicationRecord
-  belongs_to :kind
+  belongs_to :kind, optional: true
 
-  def author
-    "Agnaldo Burgo Junior"
-  end
+  # def author
+  #   "Agnaldo Burgo Junior"
+  # end
 
-  def kind_description
-    self.kind.description
-  end
+  # def kind_description
+  #   self.kind.description
+  # end
 
-  def as_json(options={})
-    super(
-      methods: [:author, :kind_description],
-      include: { kind: { only: :description} }
-    )
-  end
+  # def as_json(options={})
+  #   super(
+  #     methods: [:author, :kind_description],
+  #     include: { kind: { only: :description} }
+  #   )
+  # end
 end
